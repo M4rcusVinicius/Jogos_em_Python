4a64c96deb93360e1a5fe8344da97c70850deb1a
Marcus Vinicius <marcus.vinicius.mvap@gmail.com>
2020-09-21 13:46:17 -0300

    Commit

--- Adivinhação.py
+++ Adivinhação.py
@@ -7,7 +7,15 @@ numero_secreto = 42
 chute = int(input("Digite o seu número: "))
 print("Você digitou: ", chute)
 
+maior = chute > numero_secreto
+menor = chute < numero_secreto
+
 if (numero_secreto == chute):
     print("Você acertou!")
 else:
-    print("Você errou!")
+    if chute > numero_secreto:
+        print("O seu chute foi maior do que o número secreto!")
+    elif chute < numero_secreto:
+        print("O seu chute foi menor do que o número secreto!")
+
+print("Fim do jogo")
