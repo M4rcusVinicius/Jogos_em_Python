b97f3e331b5351897dd84d60b2dd44d2d90feae7
Marcus Vinicius <marcus.vinicius.mvap@gmail.com>
2020-09-24 15:32:19 -0300

    Palavras_corrigidas

--- Testes.py
+++ Testes.py
@@ -1,12 +1,21 @@
-arquivo = open(arquivo, "r")
+import os
+
+arquivo = open("Arquivo_Secreto//Animais.txt", "r")
 palavras = []
 
 for linha in arquivo:
-    linha = linha.strip()
+    linha = linha.strip().upper()
     palavras.append(linha)
 
 arquivo.close()
 
-n = random.randrange(0, len(palavras))
-palavra_secreta = palavras[n].upper()
-return palavra_secreta
+palavras_erradas = []
+
+alfabeto = ('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z', 'Ç')
+for palavra in palavras:
+    for letra in palavra:
+        if letra not in alfabeto:
+            palavras_erradas.append(palavra)
+
+os.system('cls') or None
+print(f"Palavras erradas : {palavras_erradas}")
